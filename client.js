73de5602144bf09df1ea697554b6a03bc5a5933e
Jiani Cao <jianicakes@gmail.com>
2022-01-19 13:41:28 -0800

    implemented event to send data & set name upon connection

--- client.js
+++ client.js
@@ -9,6 +9,11 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    conn.write("Name: JC");
+  });
+
   conn.on("data", (msg) => {
     console.log(msg);
   });
